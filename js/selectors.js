f906bb1f18ffe490f08b98bf7f158585e21f6227
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-26 18:49:31 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,14 @@
 // declare your functions here...
+function paragraphSelector(){
+  $('p');
+  return $('p');
+}
+
+function lastImageSelector(){
+  $('img:last');
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  $('')
+}
